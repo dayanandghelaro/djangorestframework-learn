6dfeec43dfef0fc558a8b39b5bff3ca48eb59908
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-31 15:31:29 +0500

    fix: make api working

--- tutorial/tutorial/quickstart/views.py
+++ tutorial/tutorial/quickstart/views.py
@@ -4,13 +4,13 @@ from rest_framework import permissions
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
-class UserViewSet(viewsets.ViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     """ API endpoint to allow users to view and edit users. """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class GroupViewSet(viewsets.ViewSet):
+class GroupViewSet(viewsets.ModelViewSet):
     """ API endpoint to allow users to view and edit groups. """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
