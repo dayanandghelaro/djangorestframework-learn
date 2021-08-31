e169d6df2cd4f888c27947bd7ff13e3a5df69e39
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-31 15:07:07 +0500

    feat: add serializers, views, urls, set as working api.

--- tutorial/tutorial/quickstart/views.py
+++ tutorial/tutorial/quickstart/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ViewSet):
+    """ API endpoint to allow users to view and edit users. """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ViewSet):
+    """ API endpoint to allow users to view and edit groups. """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'quickstart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +125,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
