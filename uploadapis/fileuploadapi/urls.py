cf111bf98487c59a9c46bfc979a47b857538e3cf
DayanandGhelaro <dayanandghelaro@A006-00592.arbisoft.com>
2021-09-17 16:56:26 +0500

    feat: update urls in fileuploadapi app

--- uploadapis/fileuploadapi/urls.py
+++ uploadapis/fileuploadapi/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from fileuploadapi.views import FileUploadViewSet
 urlpatterns = [
-    path("files/", FileUploadViewSet.as_view({"get": "list"}), name="files")
+    path("files/", FileUploadViewSet.as_view({"get": "list"}), name="files"),
+    path("add-file/", FileUploadViewSet.as_view({"post": "create"}), name="create"),
+    path("<pk>/", FileUploadViewSet.as_view({"get":"retrieve"}), name="detail"),
+    path("<pk>/update/", FileUploadViewSet.as_view({"post":"update"}), name="update"),
+    path("<pk>/delete/", FileUploadViewSet.as_view({"get":"destroy"}), name="delete"),
 ]
